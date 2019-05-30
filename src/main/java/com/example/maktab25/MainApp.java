67078f14ef059784917764a95265aeaaa5870d35
Mohsen <mohsenr7596@hotmail.com>
2019-05-30 10:40:42 +0430

    use xml type config

--- src/main/java/com/example/maktab25/MainApp.java
+++ src/main/java/com/example/maktab25/MainApp.java
@@ -4,24 +4,12 @@ package com.example.maktab25;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-import org.reflections.Reflections;
-
-import javax.persistence.Entity;
-import java.util.Set;
 
 public class MainApp {
 
     public static void main(String[] args) {
 
-        Reflections reflections = new Reflections(MainApp.class.getPackage().getName());
-        Set<Class<?>> classes = reflections.getTypesAnnotatedWith(Entity.class);
-
-        Configuration configure = new Configuration();
-
-        for (Class<?> aClass : classes) {
-            configure.addAnnotatedClass(aClass);
-        }
-
+        Configuration configure = new Configuration().configure();
         try (
                 SessionFactory factory = configure.buildSessionFactory();
                 Session session = factory.getCurrentSession()
