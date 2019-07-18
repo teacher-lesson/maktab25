e731543a47ec500ff8d78d040eeb016b535a04d7
Mohsen <mohsenr7596@hotmail.com>
2019-07-18 15:25:16 +0430

    second way insert data

--- src/main/java/com/example/domin/teacher/dao/TeacherDao.java
+++ src/main/java/com/example/domin/teacher/dao/TeacherDao.java
@@ -1,32 +1,37 @@
 package com.example.domin.teacher.dao;
 
 import com.example.domin.teacher.Teacher;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceUnit;
 
 @Repository
 public class TeacherDao {
 
-    @PersistenceContext
-    private EntityManager entityManager;
+//    @PersistenceContext
+//    private EntityManager entityManager;
 
-    
+    @PersistenceUnit
+    EntityManagerFactory entityManagerFactory;
+
+
     @Transactional
     public void create(Teacher teacher) {
-//        SessionFactory sessionFactory = entityManagerFactory.unwrap(SessionFactory.class);
-//        Session session = sessionFactory.openSession();
-//        session.save(teacher);
-//        session.close();
-
+        SessionFactory sessionFactory = entityManagerFactory.unwrap(SessionFactory.class);
+        Session session = sessionFactory.openSession();
+        session.save(teacher);
+        session.close();
+//
 //        EntityManager entityManager = entityManagerFactory.createEntityManager();
-        entityManager.persist(teacher);
-        entityManager.flush();
-        entityManager.close();
+//        entityManager.persist(teacher);
+//        entityManager.flush();
+//        entityManager.close();
     }
 
 
