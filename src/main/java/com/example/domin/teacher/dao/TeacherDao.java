a1c960a97021013649258d38895ced0df6bfa6bf
Mohsen <mohsenr7596@hotmail.com>
2019-07-18 14:35:18 +0430

    Use entity manager

--- src/main/java/com/example/domin/teacher/dao/TeacherDao.java
+++ src/main/java/com/example/domin/teacher/dao/TeacherDao.java
@@ -1,25 +1,32 @@
 package com.example.domin.teacher.dao;
 
 import com.example.domin.teacher.Teacher;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 
 @Repository
 public class TeacherDao {
 
-    private final SessionFactory sessionFactory;
 
-    public TeacherDao(SessionFactory sessionFactory) {
-        this.sessionFactory = sessionFactory;
+    private final EntityManagerFactory entityManagerFactory;
+
+    public TeacherDao(EntityManagerFactory entityManagerFactory) {
+        this.entityManagerFactory = entityManagerFactory;
     }
 
     @Transactional
     public void create(Teacher teacher) {
-        Session session = sessionFactory.openSession();
-        session.save(teacher);
-        session.close();
+//        SessionFactory sessionFactory = entityManagerFactory.unwrap(SessionFactory.class);
+//        Session session = sessionFactory.openSession();
+//        session.save(teacher);
+//        session.close();
+
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.persist(teacher);
+        entityManager.close();
     }
 
 
