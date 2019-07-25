37c2a66a375b5c176ada7443fc3b90975de84e5b
Mohsen <mohsenr7596@hotmail.com>
2019-07-25 09:36:05 +0430

    First commit

---
+++ src/main/java/com/example/blog/TestServlet.java
@@ -0,0 +1,21 @@
+package com.example.blog;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "TestServlet", urlPatterns = "/test")
+public class TestServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.getWriter().println("Hi this is servlet!");
+    }
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,11 @@
 <html>
 <body>
 <h2>Hello World!</h2>
+
+<%
+
+    response.getWriter().println("Hi this is jsp");
+
+%>
 </body>
 </html>
