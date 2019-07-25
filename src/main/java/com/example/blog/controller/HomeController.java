1bb9d6b7fdb6574731173371553dd029d8edd869
Mohsen <mohsenr7596@hotmail.com>
2019-07-25 12:10:46 +0430

    add json converter

--- src/main/java/com/example/blog/controller/HomeController.java
+++ src/main/java/com/example/blog/controller/HomeController.java
@@ -3,7 +3,9 @@ package com.example.blog.controller;
 import com.example.blog.model.User;
 import com.example.blog.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,8 +14,8 @@ public class HomeController {
     @Autowired
     private UserService userService;
 
-    @RequestMapping(value = "/user")
-    public String home() {
+    @RequestMapping(value = "/user", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public User home() {
         User user = new User();
         user.setName("Joe");
         user.setSureName("Doe");
@@ -21,7 +23,7 @@ public class HomeController {
         user.setPassword("admin");
 
         userService.create(user);
-        return "OK";
+        return user;
     }
 
 }
