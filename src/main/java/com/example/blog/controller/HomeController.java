2351c5fb77d6257caebe0b3a71749afe75ad03df
Mohsen <mohsenr7596@hotmail.com>
2019-07-25 12:01:57 +0430

    add user adder test controller

--- src/main/java/com/example/blog/controller/HomeController.java
+++ src/main/java/com/example/blog/controller/HomeController.java
@@ -1,14 +1,27 @@
 package com.example.blog.controller;
 
+import com.example.blog.model.User;
+import com.example.blog.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
-    @RequestMapping("/home")
-    public String home(){
-        return "This is test!";
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(value = "/user")
+    public String home() {
+        User user = new User();
+        user.setName("Joe");
+        user.setSureName("Doe");
+        user.setUsername("admin");
+        user.setPassword("admin");
+
+        userService.create(user);
+        return "OK";
     }
 
 }
